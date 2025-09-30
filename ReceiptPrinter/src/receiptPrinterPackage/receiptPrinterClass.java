9a2167fdd69e04b245da849fba6e5fc4baa8080f
brazkatja <katja.suvajac@gmail.com>
2025-09-30 18:17:22 +0200

    Update receiptPrinterClass.java
    added fall themed ascii art
    switched currently selected images

--- ReceiptPrinter/src/receiptPrinterPackage/receiptPrinterClass.java
+++ ReceiptPrinter/src/receiptPrinterPackage/receiptPrinterClass.java
@@ -5,15 +5,23 @@ import java.awt.print.PrinterJob;
 import java.io.Console;
 import java.util.Scanner;
 
+import receiptPrinterPackage.animals.Acorn;
+import receiptPrinterPackage.animals.Apple;
 import receiptPrinterPackage.animals.Butterfly;
 import receiptPrinterPackage.animals.Camel;
 import receiptPrinterPackage.animals.Cat;
+import receiptPrinterPackage.animals.Chesnut;
 import receiptPrinterPackage.animals.Chicken;
 import receiptPrinterPackage.animals.Crab;
 import receiptPrinterPackage.animals.Flower;
 import receiptPrinterPackage.animals.Frog;
+import receiptPrinterPackage.animals.Hedgehog;
 import receiptPrinterPackage.animals.Horse;
+import receiptPrinterPackage.animals.Leaf;
+import receiptPrinterPackage.animals.Mushroom;
+import receiptPrinterPackage.animals.Pumpkin;
 import receiptPrinterPackage.animals.Seahorse;
+import receiptPrinterPackage.animals.Squirel;
 import receiptPrinterPackage.animals.Star;
 
 public class receiptPrinterClass {
@@ -34,7 +42,14 @@ public class receiptPrinterClass {
 				System.out.println();
 				System.out.println("---------------------------------------------------");
 				System.out.println();
-				printAppropriateAnimalAndSetVariable(input);
+				if (input.toString().toLowerCase().equals("marija") 
+						|| input.toString().toLowerCase().equals("petra")) {
+					System.out.println(Flower.toConsole);
+					animalToPrint = Flower.toPrint;
+					
+				} else {
+					printAppropriateAnimalAndSetVariable(input);
+				}
 				System.out.println();
 				
 				try {
@@ -63,44 +78,44 @@ public class receiptPrinterClass {
 		}
 		switch(integer%10) {
 			case 1:
-				System.out.println(Camel.toConsole);
-				animalToPrint = Camel.toPrint;
+				System.out.println(Pumpkin.toConsole);
+				animalToPrint = Pumpkin.toPrint;
 				break;
 			case 2:
 				System.out.println(Cat.toConsole);
 				animalToPrint = Cat.toPrint;
 				break;
 			case 3:
-				System.out.println(Chicken.toConsole);
-				animalToPrint = Chicken.toPrint;
+				System.out.println(Mushroom.toConsole);
+				animalToPrint = Mushroom.toPrint;
 				break;
 			case 4:
-				System.out.println(Flower.toConsole);
-				animalToPrint = Flower.toPrint;
+				System.out.println(Hedgehog.toConsole);
+				animalToPrint = Hedgehog.toPrint;
 				break;
 			case 5:
-				System.out.println(Frog.toConsole);
-				animalToPrint = Frog.toPrint;
+				System.out.println(Chesnut.toConsole);
+				animalToPrint = Chesnut.toPrint;
 				break;
 			case 6:
-				System.out.println(Horse.toConsole);
-				animalToPrint = Horse.toPrint;
+				System.out.println(Leaf.toConsole);
+				animalToPrint = Leaf.toPrint;
 				break;
 			case 7:
-				System.out.println(Seahorse.toConsole);
-				animalToPrint = Seahorse.toPrint;
+				System.out.println(Acorn.toConsole);
+				animalToPrint = Acorn.toPrint;
 				break;
 			case 8:
-				System.out.println(Butterfly.toConsole);
-				animalToPrint = Butterfly.toPrint;
+				System.out.println(Squirel.toConsole);
+				animalToPrint = Squirel.toPrint;
 				break;
 			case 9:
 				System.out.println(Star.toConsole);
 				animalToPrint = Star.toPrint;
 				break;
 			default:
-				System.out.println(Crab.toConsole);
-				animalToPrint = Crab.toPrint;
+				System.out.println(Apple.toConsole);
+				animalToPrint = Apple.toPrint;
 				break;
 		}
 		
@@ -132,7 +147,7 @@ public class receiptPrinterClass {
 			+ "    _.                      :\r\n"
 			+ "                            ;`-\r\n"
 			+ "                           :\\\r\n"
-			+ "                           ;\\r\\n\"";
+			+ "                           ;\\r\n\"";
 	
 	private static String wolfey2 = "\r\n"
 			+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣷⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
